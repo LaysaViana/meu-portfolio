c5ef138cb58f87bd900c0068646494e25980c595
Laysa <laysa.viana@outlook.com>
2025-10-16 15:32:17 -0300

    chore: adiciona react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { Toaster } from 'react-hot-toast';
+
 import './index.css';
 import Root from './Root';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Root />
+    <QueryClientProvider client={queryClient}>
+      <Root />
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          style: {
+            background: '#333',
+            color: '#fff',
+            borderRadius: '8px',
+          },
+        }}
+      />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
